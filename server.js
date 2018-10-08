8c30e6b4f018c8d8a8c9239e60ae2a32f69d5cd6
Tomas Kovtun <tomaskovtun@yahoo.com>
2018-10-08 22:14:51 +0300

    new commit wassaaaaap

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const options = {
 webpackDevServer.addDevServerEntrypoints(config, options);
 const compiler = webpack(config);
 const server = new webpackDevServer(compiler, options);
-
+// new line
 server.listen(5000, 'localhost', () => {
   console.log('dev server listening on port 5000');
 });
